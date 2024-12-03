f974798cf8514a70cbf80f91528cb410cfbb4282
Mohd Hammad Siddiqui <mdhammad139@gmail.com>
2024-12-03 15:57:45 +0530

    chanegd the ip in main

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from typing import Union
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 from controllers.student_controller import router as studentRouter
 import uvicorn
+
 app = FastAPI()
+
+# Include the student router
 app.include_router(studentRouter)
+
 if __name__ == "__main__":
     uvicorn.run(
         "main:app", 
-        host="127.0.0.1", 
-        port=8000,  #
+        host="0.0.0.0",  # Change from "127.0.0.1" to "0.0.0.0"
+        port=8000,  
         log_level="info",  
         reload=False,
     )
-
